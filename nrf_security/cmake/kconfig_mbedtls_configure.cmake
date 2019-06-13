01d43b1ebd11dd592f8f309261750b5ceb018b8a
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2019-06-13 14:10:03 +0200

    nrf_security: Changing internal macros and functions
    Note: This is shown as single commit for clarity, not bisectable
    -Renamed function 'PREPEND' to 'append_with_prefix'
    -Added macro 'kconfig_mbedtls_config_alt' that sets _ALT flag if mbed TLS
     function is enabled as glued or in CC310 backend
    -Added macro 'configure_file_ifdef' to conditionaly configure a file
    -Added macro 'kconfig_mbedtls_debug' to print debug information
     (build with -DDEBUG_KCONFIG_MBEDTLS=1)
    -Changed macro 'konfig_mbedtls_config' to create vanilla-only/CC310-only
     defines
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
    Signed-off-by: Kamil Sroka <kamil.sroka@nordicsemi.no>

--- nrf_security/cmake/kconfig_mbedtls_configure.cmake
+++ nrf_security/cmake/kconfig_mbedtls_configure.cmake
@@ -7,35 +7,105 @@
 # on the Kconfig settings.
 # When all values has been processed, the mbedtls config file will be generated.
 
-# Internal macro which will enable the define in mbed TLS config file based on
-# the correspong Kconfig setting.
-# Any additional arguments given are dependencies to to first argument, such
-# that those arguments will be set to TRUE if first argument is TRUE.
-macro(kconfig_mbedtls_config mbedtls_config_define)
-  if (CONFIG_${mbedtls_config_define} OR
-      CONFIG_CC310_${mbedtls_config_define} OR
-      CONFIG_VANILLA_${mbedtls_config_define})
-    set(${mbedtls_config_define} TRUE)
-    foreach(arg ${ARGN})
-      set(${arg} TRUE)
-    endforeach()
+
+#
+# Internal macro that shows debug output in for nrf_security related
+# configurations when Cmake is run with -DDEBUG_KCONFIG_MBEDTLS=1
+#
+macro(kconfig_mbedtls_debug)
+  if(DEBUG_KCONFIG_MBEDTLS)
+    message("${ARGN}")
   endif()
 endmacro()
 
+#
+# Internal macro that shows source files in a mbed TLS library (lib_name)
+# when CMake is run with -DDEBUG_KCONFIG_MBEDTLS=1
+# Note that inspecting the zephyr.map file is safer
+#
+macro(kconfig_mbedtls_debug_list_target_files lib_name)
+  if(DEBUG_KCONFIG_MBEDTLS)
+    message("--- Sources in ${lib_name} ---")
+    get_target_property(file_list ${lib_name} SOURCES)
+    foreach(line IN LISTS file_list)
+      message("${line}")
+    endforeach()
+    message("--- End sources in ${lib_name} ---")
+  endif()
+endmacro()
+
+#
+# Internal macro which will enable the define in mbed TLS config file based on
+# the correspong Kconfig setting.
+# It will additionally set VANILLA_ONLY_MBEDTLS_XXXX in case no other
+# backends is enabling the same feature
+#
+macro(kconfig_mbedtls_config base)
+  if (CONFIG_${base} OR
+      CONFIG_CC310_${base} OR
+      CONFIG_VANILLA_${base})
+    kconfig_mbedtls_debug("Setting ${base} to TRUE")
+    set(${base} TRUE)
+  endif()
+  if ((CONFIG_${base} AND NOT CONFIG_CC310_BACKEND) OR
+      (CONFIG_VANILLA_${base} AND NOT CONFIG_CC310_${base}))
+    kconfig_mbedtls_debug("Setting VANILLA_ONLY_${base} to TRUE")
+    set(VANILLA_ONLY_${base} TRUE)
+  endif()
+  if (CONFIG_${base} AND NOT CONFIG_MBEDTLS_VANILLA_BACKEND)
+    kconfig_mbedtls_debug("Setting CC310_${base} to TRUE")
+    set(CC310_${base} TRUE)
+  endif()
+  if (CONFIG_CC310_${base})
+    kconfig_mbedtls_debug("Setting CC310_${base} to TRUE")
+    set(CC310_${base} TRUE)
+  endif()
+endmacro()
+
+#
+# Internal macro which will enable the define in mbed TLS config file based on
+# the correspong Kconfig setting (for _ALT flags)
+# Any additional arguments given are dependencies to to first argument, such
+# that those arguments will be set to TRUE if first argument is TRUE.
+#
+macro(kconfig_mbedtls_config_alt base)
+  # Note that we don't add CONFIG_ to ${base}_C as it is not used in mbed TLS
+  # config file
+  if ((${base}_C AND (CONFIG_CC310_BACKEND AND
+      NOT CONFIG_MBEDTLS_VANILLA_BACKEND)) OR
+      CONFIG_CC310_${base}_C OR
+      CONFIG_GLUE_${base}_C)
+    kconfig_mbedtls_debug("ALT Setting ${base}_ALT to TRUE")
+    set(${base}_ALT TRUE)
+  endif()
+endmacro()
+
+#
 # Internal macro which will enable the define dependencies in mbed TLS config
 # file if the any backend has enabled the setting in Kconfig specified by first
 # argument.
+#
 macro(mbedtls_config_define_depends mbedtls_config)
   set(${mbedtls_config} TRUE)
   foreach(arg ${ARGN})
-  if (NOT (CONFIG_${arg} OR
-           CONFIG_CC310_${arg} OR
-           CONFIG_VANILLA_${arg}))
-    unset(${mbedtls_config})
-  endif()
+    if (NOT (CONFIG_${arg} OR
+            CONFIG_CC310_${arg} OR
+            CONFIG_VANILLA_${arg}))
+      unset(${mbedtls_config})
+      break()
+    endif()
   endforeach()
 endmacro()
 
+#
+# Internal macro to configure file if mbed TLS config is set
+#
+macro(configure_file_ifdef mbedtls_config)
+  if (${mbedtls_config})
+    kconfig_mbedtls_debug("Configure file: ${ARGV1}")
+    configure_file(${ARGV1} ${ARGV2} ${ARGV3})
+  endif()
+endmacro()
 # If vanilla is not enabled, then select ALT impl to ensure rest of dependent
 # functionality is still compiled.
 if (NOT CONFIG_MBEDTLS_VANILLA_BACKEND)
