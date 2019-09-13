7fb6450b772b842a2759a22519ed6c1b9856f61c
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2019-09-13 13:10:05 +0200

    Add Kconfig option CONFIG_SHORT_WCHAR_T
    -Added Kconfig option to indicate build with -fshort-wchar
    -Updated nrfxlib_caculate_lib_path to also select short-wchar
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- common.cmake
+++ common.cmake
@@ -10,20 +10,29 @@ if(__NRFXLIB_COMMON_CMAKE__)
 endif()
 set(__NRFXLIB_COMMON_CMAKE__ TRUE)
 
+#
+# Build the path of the library taking in
+# -Arch type
+# -Float ABI
+# -wchar_t size
+#
 function(nrfxlib_calculate_lib_path lib_path)
-  if(CONFIG_FLOAT)
-	if(CONFIG_FP_HARDABI)
-      set(float_dir hard-float)
-	elseif(CONFIG_FP_SOFTABI)
-      set(float_dir softfp-float)
-	else()
-      assert(0 "Unreachable code")
-	endif()
-  else()
-	set(float_dir soft-float)
-  endif()
-
+  # Add Arch type
   assert(GCC_M_CPU "GCC_M_CPU must be set to find correct lib.")
-
-  set(${lib_path} lib/${GCC_M_CPU}/${float_dir} PARENT_SCOPE)
+  # Set floating ABI
+  if(CONFIG_FLOAT)
+    if(CONFIG_FP_HARDABI)
+        set(float_dir hard-float)
+    elseif(CONFIG_FP_SOFTABI)
+        set(float_dir softfp-float)
+    else()
+        assert(0 "Unreachable code")
+    endif()
+  else()
+	  set(float_dir soft-float)
+  endif()
+  if (CONFIG_SHORT_WCHAR_T)
+    set(short_wchar "/short-wchar")
+  endif()
+  set(${lib_path} "lib/${GCC_M_CPU}/${float_dir}${short_wchar}" PARENT_SCOPE)
 endfunction()
