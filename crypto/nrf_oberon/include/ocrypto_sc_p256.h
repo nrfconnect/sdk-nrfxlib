9ec8d9e6d83242071ce14d9fdcb28d52e7b62090
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2019-12-02 09:08:37 +0100

    crypto: Updated nrf_oberon to version 3.0.3
    Adding nrf_oberon v3.0.3
    Removing nrf_oberon v3.0.2
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

---
+++ crypto/nrf_oberon/include/ocrypto_sc_p256.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef OCRYPTO_SC_P256_H
+#define OCRYPTO_SC_P256_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+// P-256 scalar modulo group order
+/**@cond */
+typedef struct
+{
+    uint32_t w[8]; // little endian
+}
+ocrypto_sc_p256;
+/**@endcond */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
