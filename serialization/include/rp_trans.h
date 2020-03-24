doki-nordic/nrfxlib-1

serialization: Add serialization library
This commit provides Os independent Remote Procedure
Serialization library.

Signed-off-by: Kamil Gawor <Kamil.Gawor@nordicsemi.no>

serialization/include/rp_trans.h
@@ -1,0 +1,16 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef RP_TRANS_H_
+#define RP_TRANS_H_
+
+#if defined(CONFIG_RMPSG_TRANSPORT)
+
+#include <trans_rpmsg.h>
+
+#endif  /* CONFIG_RMPSG_TRANSPORT */
+
+#endif  /* RP_TRANS_H_ */

nrfconnect/sdk-nrfxlib

serialization/include/rp_trans.h
@@ -1,0 +1,16 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef RP_TRANS_H_
+#define RP_TRANS_H_
+
+#if defined(CONFIG_RPMSG_TRANSPORT)
+
+#include <trans_rpmsg.h>
+
+#endif  /* CONFIG_RMPSG_TRANSPORT */
+
+#endif  /* RP_TRANS_H_ */