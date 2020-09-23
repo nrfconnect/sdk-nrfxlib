d37fd889b89bc62b280784071dc489a6d26d8bd5
Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>
2020-09-23 11:30:27 +0200

    openthread: Create separate libraries for differend crypto backends
    Adjust install_openthread_libraries target to copy compiled libraries
    to the right location taking in consideration used nrf_security backend
    and OpenThread feature option set.
    Add option to strip debug symbols from the prebuild libraries to lower
    their size.
    Check OpenThead dependencies when the app links OpenThread prebuild
    libraries.
    
    Signed-off-by: Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>

--- common.cmake
+++ common.cmake
@@ -4,12 +4,6 @@
 # SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
 #
 
-# Header guard
-if(__NRFXLIB_COMMON_CMAKE__)
-  return()
-endif()
-set(__NRFXLIB_COMMON_CMAKE__ TRUE)
-
 #
 # Build the path of the library taking in
 # -Arch type
@@ -45,4 +39,27 @@ function(nrfxlib_calculate_lib_path lib_path)
     set(short_wchar "/short-wchar")
   endif()
   set(${lib_path} "lib/${arch_soc_dir}/${float_dir}${short_wchar}" PARENT_SCOPE)
+endfunction()
+
+function(get_mbedtls_dir ARM_MBEDTLS_PATH)
+  if(EXISTS ${ARM_MBEDTLS_PATH})
+  # Do nothing, just use the provided path
+  elseif(WEST)
+  ## Use `west` to find the mbedtls tree we use (this is not the
+  ## mbedtls tree distributed as a zephyr module).
+    execute_process(
+      COMMAND ${PYTHON_EXECUTABLE} -c
+      "from west.manifest import Manifest; manifest = Manifest.from_file(); print(manifest.get_projects(['mbedtls-nrf'])[0].posixpath)"
+      OUTPUT_VARIABLE west_project_output
+      RESULT_VARIABLE result
+    )
+    string(REGEX REPLACE "[\r\n]+" "" ${ARM_MBEDTLS_PATH} "${west_project_output}")
+    if(${result})
+      message(FATAL_ERROR "Failed to find mbedtls, cannot build security libraries")
+    endif()
+  else()
+    message(FATAL_ERROR "west not installed, please provide ARM_MBEDTLS_PATH to CMake to support security libraries")
+  endif()
+
+  set(ARM_MBEDTLS_PATH ${ARM_MBEDTLS_PATH} PARENT_SCOPE)
 endfunction()
