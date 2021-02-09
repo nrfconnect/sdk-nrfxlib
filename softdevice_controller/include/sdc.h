52f4b7163fb4c1b7bd4b4c8c79142198b1a9a341
Timothy Keys <timothy.keys@nordicsemi.no>
2021-02-09 14:30:30 +0100

    softdevice_controller: rev e5c79cd991001d66eafb6e7b982f420df16093c8
    CHANGELOG.rst contains the list of changes.
    
    Signed-off-by: Timothy Keys <timothy.keys@nordicsemi.no>

--- softdevice_controller/include/sdc.h
+++ softdevice_controller/include/sdc.h
@@ -74,7 +74,7 @@ extern "C" {
  */
 
 /** @brief Maximum number of bytes required per master link for the default buffer configuration. */
-#define SDC_MEM_DEFAULT_MASTER_LINK_SIZE 788
+#define SDC_MEM_DEFAULT_MASTER_LINK_SIZE 878
 
 /** @brief Maximum number of bytes required per slave link for the default buffer configuration. */
 #define SDC_MEM_DEFAULT_SLAVE_LINK_SIZE 940
