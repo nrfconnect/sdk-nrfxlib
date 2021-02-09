3abbecd1c213313c2a6571eac5b08e18ac09f811
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2021-02-09 11:58:43 +0100

    nrf_security: cleanup of mbedtls shared library
    Fixes: NCSDK-6991
    
    This commit contains no functional changes and is a pure cleanup of the
    CMake code.
    
    It creates the function `nrf_security_shared_library()` which removes
    several lines of duplicated code, and as part of this cleanup the use
    of empty_file.c has been removed.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- nrf_security/cmake/nrf_glue_library.cmake
+++ nrf_security/cmake/nrf_glue_library.cmake
@@ -61,3 +61,89 @@ function(nrf_security_glue_library)
   endif()
   nrf_security_debug_list_target_files(${ZEPHYR_CURRENT_LIBRARY})
 endfunction()
+
+
+function(nrf_security_shared_library)
+  cmake_parse_arguments(LIB "" "NAME" "" ${ARGN})
+  set(sources "")
+  set(ARGN_LOCAL ${ARGN})
+
+  set(index 0)
+  set(prev_index 0)
+  set(length -1)
+  foreach(arg ${ARGN_LOCAL})
+    if(${arg} STREQUAL FILES)
+      list(APPEND FILES_START_INDICES ${index})
+      if(prev_index)
+        math(EXPR  length "${index} - ${prev_index}")
+        set(FILES_INDEX_${prev_index}_LENGTH ${length})
+      endif()
+      set(prev_index ${index})
+    endif()
+    math(EXPR  index "${index} + 1")
+  endforeach()
+  set(FILES_INDEX_${prev_index}_LENGTH -1)
+
+  foreach(index ${FILES_START_INDICES})
+    list(SUBLIST ARGN_LOCAL ${index} ${FILES_INDEX_${index}_LENGTH} SUB_LIST)
+
+    # Create sublist and parse sub-args.
+    cmake_parse_arguments(SOURCE_LIST "" "" "FILES;INCLUDE;EXCLUDE" ${SUB_LIST})
+
+    list(GET SOURCE_LIST_FILES 0 file)
+    get_filename_component(file_name ${file} NAME_WE)
+    string(TOUPPER ${file_name} SETTING_UPPER)
+
+    if(CONFIG_MBEDTLS_${SETTING_UPPER}_C)
+      set(use_file True)
+      foreach(backend ${SOURCE_LIST_INCLUDE})
+        if(CONFIG_${backend}_MBEDTLS_${SETTING_UPPER}_C)
+          # include file
+          set(use_file True)
+        endif()
+      endforeach()
+
+      foreach(backend ${SOURCE_LIST_EXCLUDE})
+        if(CONFIG_${backend}_MBEDTLS_${SETTING_UPPER}_C)
+          # exclude file
+          set(use_file False)
+        endif()
+      endforeach()
+
+      if(${use_file})
+        nrf_security_debug("Adding to shared: ${SETTING_UPPER}")
+        list(APPEND sources ${SOURCE_LIST_FILES})
+      endif()
+    endif()
+  endforeach()
+
+  if(NOT sources STREQUAL "")
+    zephyr_library_named(${LIB_NAME})
+    target_sources(${LIB_NAME} PRIVATE ${sources})
+
+    #
+    # Add specific includes for threading and platform_cc3xx
+    #
+    if(TARGET platform_cc3xx)
+      target_include_directories(${LIB_NAME} PRIVATE
+        ${mbedcrypto_glue_include_path}/threading
+        $<TARGET_PROPERTY:platform_cc3xx,BASE_INCLUDE_PATH>
+      )
+    endif()
+
+    target_compile_options(${LIB_NAME} PRIVATE ${TOOLCHAIN_C_FLAGS})
+    target_ld_options(${LIB_NAME} PRIVATE ${TOOLCHAIN_LD_FLAGS})
+
+    # Where are ${common_includes} and ${config_include} defined ?
+    target_include_directories(${LIB_NAME} PRIVATE
+        ${common_includes}
+        ${config_include}
+    )
+
+    target_compile_definitions(${LIB_NAME} PRIVATE
+      -DMBEDTLS_CONFIG_FILE="nrf-config-noglue.h"
+    )
+
+    nrf_security_debug_list_target_files(${LIB_NAME})
+  endif()
+endfunction()
