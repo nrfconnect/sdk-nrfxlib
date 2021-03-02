26ea2fdf71162ffb18e95f55e753217b91a1e3f3
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2021-03-02 15:23:45 +0100

    nrf_security: Refactoring mbedcrypto build
    -NOTE: Needs to be in a single commit for bisectability
    -Part of unified mbedcrypto static target work for TF-M
    -Part of nrf_security CMake beautification
    -Restructure CMakeLists.txt in folders
    -Adds mbedcrypto, mbedx509, mbedtls libraries
    -Adds dependencies for TF-M build
    -Removes dependency on zepyhyr_interface for mbed TLS libraries
    -Reworks rename/strip functionality
    
    ref: NCSDK-7907
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- nrf_security/src/mbedcrypto_glue/oberon/oberon_glue.cmake
+++ nrf_security/src/mbedcrypto_glue/oberon/oberon_glue.cmake
@@ -6,39 +6,13 @@
 nrf_security_debug("######### Creating oberon glue library #########")
 
 #
-# Create the Oberon glue library
+# Create the mbedcrypto_glue_oberon
 #
-zephyr_library_named(mbedcrypto_glue_oberon)
-
-#
-# Adding Oberon backend glue files
-#
-zephyr_library_sources_ifdef(CONFIG_GLUE_OBERON_MBEDTLS_AES_C
-  ${CMAKE_CURRENT_LIST_DIR}/aes_oberon.c
-)
-
-zephyr_library_sources_ifdef(CONFIG_GLUE_OBERON_MBEDTLS_CCM_C
-  ${CMAKE_CURRENT_LIST_DIR}/ccm_oberon.c
-)
-
-zephyr_library_sources(${ZEPHYR_BASE}/misc/empty_file.c)
-zephyr_library_compile_definitions(MBEDTLS_BACKEND_PREFIX=oberon)
-zephyr_library_link_libraries(mbedtls_common_glue)
-
-add_dependencies(mbedcrypto_glue_oberon mbedcrypto_oberon_renamed)
-
-nrf_security_debug_list_target_files(mbedcrypto_glue_oberon)
-
-#
-# Rename the external symbols as referenced through the glue files
-# The APIs will match oberon_mbedtls_<xxxx> after this.
-#
-
-add_custom_command(
-  TARGET mbedcrypto_glue_oberon
-  POST_BUILD
-  COMMAND ${CMAKE_OBJCOPY}
-          --redefine-syms ${CMAKE_CURRENT_BINARY_DIR}/symbol_rename_oberon.txt
-          $<TARGET_FILE:mbedcrypto_glue_oberon>
-  DEPENDS mbedcrypto_oberon_renamed
+nrf_security_library_glue(BACKEND oberon
+  FILES
+    ${CMAKE_CURRENT_LIST_DIR}/aes_oberon.c
+    ${CMAKE_CURRENT_LIST_DIR}/ccm_oberon.c
+  LINK_LIBRARIES
+    mbedcrypto_common_glue
+    mbedcrypto_oberon
 )
