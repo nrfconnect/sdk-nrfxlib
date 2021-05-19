d298c6acf58afad024fa95d5774ca09ef94faa76
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2021-05-19 12:01:43 +0200

    crypto: Adding option for no mutex when MULTITHREADING is off
    -Added additional source file that does not initialize RTOS mutex
     in the CryptoCell runtime library
    -Added build-asserts to protect against misuse/misconfiguration
    
    ref: NCSDK-9650
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

---
+++ crypto/nrf_cc312_platform/src/nrf_cc3xx_platform_no_mutex_zephyr.c
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#include <zephyr.h>
+
+BUILD_ASSERT(!IS_ENABLED(CONFIG_MULTITHREADING),
+	"This file is intended for single-threading, but multi-threading is enabled. "
+	"Please check your config build configuration!");
+
+/** @brief Function to initialize the nrf_cc3xx_platform mutex APIs
+ */
+void nrf_cc3xx_platform_mutex_init(void)
+{
+	// No thread-safe mutexes are required
+}
