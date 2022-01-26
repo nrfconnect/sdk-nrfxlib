309671a91f9d7faebbfeeeeee24e2219e1f0f8c2
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-01-26 13:36:58 +0100

    nrf_security: Add workaround for oberon_verify_message
    Add workaround for oberon_verify_message not supporting using
    key attributes with key pair. Export a public-only key
    before calling oberon_verify_message.
    
    NCSDK-13486
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- nrf_security/src/psa_crypto_driver_wrappers.c
+++ nrf_security/src/psa_crypto_driver_wrappers.c
@@ -237,6 +237,35 @@ psa_status_t psa_driver_wrapper_verify_message(
                 return( status );
 #endif /* PSA_CRYPTO_DRIVER_CC3XX */
 #if defined(PSA_CRYPTO_DRIVER_OBERON)
+        {
+            /* Workaround NCSDK-13486
+             * oberon_verify_message does not support PSA_KEY_TYPE_CATEGORY_KEY_PAIR.
+             * Export a key that is PSA_KEY_TYPE_CATEGORY_PUBLIC_KEY instead.
+             */
+            psa_key_attributes_t attributes_copy = *attributes;
+            psa_key_type_t key_type = psa_get_key_type(&attributes_copy);
+
+            size_t exported_length = 0;
+            uint8_t exported[PSA_KEY_EXPORT_ECC_PUBLIC_KEY_MAX_SIZE(256)];
+
+            if (PSA_KEY_TYPE_IS_KEY_PAIR(key_type)) {
+                mbedtls_svc_key_id_t key = psa_get_key_id(&attributes_copy);
+
+                status = psa_export_public_key(key, exported, sizeof(exported),
+                                               &exported_length);
+                if (status != PSA_SUCCESS) {
+                    return status;
+                }
+
+                key_type &= ~PSA_KEY_TYPE_CATEGORY_FLAG_PAIR;
+                psa_set_key_type(&attributes_copy, key_type);
+
+                key_buffer_size = exported_length;
+                key_buffer = exported;
+
+                attributes = &attributes_copy;
+            }
+
             status = oberon_verify_message(
                         attributes,
                         key_buffer,
@@ -249,6 +278,7 @@ psa_status_t psa_driver_wrapper_verify_message(
             /* Declared with fallback == true */
             if( status != PSA_ERROR_NOT_SUPPORTED )
                 return( status );
+        }
 #endif /* PSA_CRYPTO_DRIVER_OBERON */
 #if defined(PSA_CRYPTO_DRIVER_TEST)
             status = mbedtls_test_transparent_signature_verify_message(
