4627335748fc3c0a777ec9532bc4c55d49f1472d
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-04-25 22:00:15 +0200

    crypto: nrf_oberon: Fix nits empty line at end of function
    Fix nits: empty line at end of function.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- crypto/nrf_oberon/core/library/platform_util.c
+++ crypto/nrf_oberon/core/library/platform_util.c
@@ -142,4 +142,3 @@ struct tm *mbedtls_platform_gmtime_r( const mbedtls_time_t *tt,
 #if defined(MBEDTLS_TEST_HOOKS)
 void (*mbedtls_test_hook_test_fail)( const char *, int, const char *);
 #endif /* MBEDTLS_TEST_HOOKS */
-
