28795d57826c77526bd5f77e61e45bbad2084f36
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-04-28 12:49:57 +0200

    crypto: nrf_oberon: Add ocrypto ocrypto_doxygen header file
    Add ocrypto_doxygen header file, which defines the doxygen group
    ocrypto.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ crypto/nrf_oberon/include/ocrypto_doxygen.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 - 2023 Nordic Semiconductor ASA
+ * Copyright (c) since 2013 Oberon microsystems AG
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/**@file
+ * @defgroup ocrypto Oberon cryptographic library
+ * @{
+ * @brief Highly optimized cryptographic algorithm implementation for Cortex-M0, Cortex-M4,
+ * and Cortex-M33. Created by Oberon, under distribution license with Nordic Semiconductor ASA.
+ * @}
+ *
+
+/*
+ * @defgroup ocrypto_chacha_poly ChaCha20-Poly1305
+ * @ingroup ocrypto
+ * @{
+ * @brief ChaCha20-Poly1305 is an authenticated encryption algorithm with optional
+ *        additional authenticated data developed by Daniel J.Bernstein.
+ * @}
+ */
+
+/** @} */
