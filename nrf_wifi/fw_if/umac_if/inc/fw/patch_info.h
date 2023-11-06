b9008a106da9822e2cd0b679e2abea420cb8339b
Karun Kumar Eagalapati <karun.kumar@nordicsemi.no>
2023-11-06 14:57:31 +0530

    nrf_wifi: Update RPU patch to rev#1c05858e8b7
    UMAC:
        [CAL-3331]  : Tx packet injector changes.
        [SHEL-1780] : Disabled DFS channel with the feature flag.
    
    LMAC:
        Deagg bug fix in firmware.
        Patch scripts updated for generating  bin instead of header files.
    
    Signed-off-by: Karun Kumar Eagalapati <karun.kumar@nordicsemi.no>

--- nrf_wifi/fw_if/umac_if/inc/fw/patch_info.h
+++ nrf_wifi/fw_if/umac_if/inc/fw/patch_info.h
@@ -48,6 +48,6 @@ struct nrf70_fw_image_info {
 #define RPU_FAMILY         (1)
 #define RPU_MAJOR_VERSION   (2)
 #define RPU_MINOR_VERSION   (9)
-#define RPU_PATCH_VERSION   (0)
+#define RPU_PATCH_VERSION   (1)
 
 #endif /* _PATCH_INFO_H */
