a8cd38dc14fc65a1dd3ce741610202c87e2a1d29
Vivekananda Uppunda <vivekananda.uppunda@nordicsemi.no>
2023-11-15 10:23:16 +0530

    nrf_wifi: Remove packet filter setting code for sniffer mode
    This set of changes removes the filter setting code for sniffer mode
    from the TX injection changes. The changes will be brought back in
    along with sniffer mode RX code changes
    
    Signed-off-by: Vivekananda Uppunda <vivekananda.uppunda@nordicsemi.no>

--- nrf_wifi/fw_if/umac_if/src/event.c
+++ nrf_wifi/fw_if/umac_if/src/event.c
@@ -935,16 +935,6 @@ static enum nrf_wifi_status umac_process_sys_events(struct nrf_wifi_fmac_dev_ctx
 		}
 		status = NRF_WIFI_STATUS_SUCCESS;
 		break;
-	case NRF_WIFI_EVENT_FILTER_SET_DONE:
-		struct nrf_wifi_event_raw_config_filter *filter_event;
-
-		filter_event = (struct nrf_wifi_event_raw_config_filter *)sys_head;
-		if (!filter_event->status) {
-			def_dev_ctx->vif_ctx[filter_event->if_index]->packet_filter =
-								filter_event->filter;
-		}
-		status = NRF_WIFI_STATUS_SUCCESS;
-		break;
 #endif /* CONFIG_NRF700X_RAW_DATA_TX */
 	default:
 		nrf_wifi_osal_log_err(fmac_dev_ctx->fpriv->opriv,
