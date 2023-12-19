89559a85ae11380c7dff81e80babed0ec0162945
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-12-20 00:16:56 +0530

    nrf_wifi: Fix unaligned access warning
    The address of the char array is directly used by HAL to program to
    nRF70, so, to avoid unaligned source address warning, ensure the
    4-byte address alignment.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- nrf_wifi/hw_if/hal/inc/fw/pack_def.h
+++ nrf_wifi/hw_if/hal/inc/fw/pack_def.h
@@ -5,12 +5,19 @@
  */
 
 /**
- * @brief phy init config parameters. These are passed to phy at init.
+ * @brief Toolchain agnostic packing definitions
  */
 
 #ifndef __PACK_DEF__
 #define __PACK_DEF__
 
-#define __NRF_WIFI_PKD __attribute__((__packed__))
+#ifdef __ZEPHYR__
+#include <zephyr/toolchain.h>
+#elif __KERNEL__
+#include <linux/compiler_attributes.h>
+#endif
+
+#define __NRF_WIFI_PKD __packed
+#define __NRF_WIFI_ALIGN_4 __aligned(4)
 
 #endif
