f5ff7c4f90542fc1e0e1c8cfdeba97be87299196
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2024-05-14 00:24:08 +0530

    nrf_wifi: Implement RPU recovery trigger
    When a watchdog interrupt is received, verify the conditions and see if
    a RPU recovery needs to be performed or not.
    
    For now we just check if host as asserted WAKEUP_NOW or not, and if not
    then watchdog interrupt is unexpected and needs a recovery.
    
    HAL processes the IRQ and decides if it needs a recovery or not, and
    FMAC relays the recovery for the native code to implement the recovery
    based on the OS.
    
    Implements SHEL-2726.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- nrf_wifi/fw_if/umac_if/inc/default/fmac_api.h
+++ nrf_wifi/fw_if/umac_if/inc/default/fmac_api.h
@@ -1036,6 +1036,14 @@ bool nrf_wifi_util_is_rawpktmode_enabled(struct nrf_wifi_fmac_vif_ctx *vif);
  *@retval      WIFI_NRF_STATUS_FAIL On failure
  */
 enum nrf_wifi_status nrf_wifi_check_mode_validity(unsigned char mode);
+
+#if defined(CONFIG_NRF_WIFI_RPU_RECOVERY) || defined(__DOXYGEN__)
+/** @cond INTERNAL_HIDDEN */
+enum nrf_wifi_status nrf_wifi_fmac_rpu_recovery_callback(void *mac_dev_ctx,
+						void *event_data,
+						unsigned int len);
+/** @endcond */
+#endif /* CONFIG_NRF_RPU_RECOVERY */
 /**
  * @}
  */
