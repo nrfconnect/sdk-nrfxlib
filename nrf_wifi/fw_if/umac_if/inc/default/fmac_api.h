9b1bb42774bac0eae5cd40dbfff5e87a933d0906
Ajay Parida <ajay.parida@nordicsemi.no>
2024-06-06 17:04:31 +0530

    nrf_wifi: Support to configure quiet period
    [SHEL-2817] Configure user provided quiet period to firmware.
    
    Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>

--- nrf_wifi/fw_if/umac_if/inc/default/fmac_api.h
+++ nrf_wifi/fw_if/umac_if/inc/default/fmac_api.h
@@ -1045,6 +1045,22 @@ enum nrf_wifi_status nrf_wifi_fmac_rpu_recovery_callback(void *mac_dev_ctx,
 						unsigned int len);
 /** @endcond */
 #endif /* CONFIG_NRF_RPU_RECOVERY */
+
+/**
+ * @brief Configure WLAN quiet period.
+ * @param fmac_dev_ctx Pointer to the UMAC IF context for a RPU WLAN device.
+ * @param if_idx Index of the interface on which power management is to be set.
+ * @param quite period Quiet period in seconds to be set.
+ *
+ * This function is used to send a command (%NRF_WIFI_UMAC_CMD_CONFIG_QUIET_PERIOD to:
+ *         - The RPU firmware to set quiet period.
+ *
+ *@retval      NRF_WIFI_STATUS_SUCCESS On success
+ *@retval      NRF_WIFI_STATUS_FAIL On failure to execute command
+ */
+enum nrf_wifi_status nrf_wifi_fmac_set_quiet_period(void *fmac_dev_ctx,
+						    unsigned char if_idx,
+						    unsigned int quiet_period);
 /**
  * @}
  */
