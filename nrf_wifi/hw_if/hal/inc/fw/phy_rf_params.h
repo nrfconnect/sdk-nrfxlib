c286166e19df783cf724d2032e86fc19c1891370
Praveen Kankipati <Praveen.Kankipati@nordicsemi.no>
2024-06-07 15:00:10 +0530

    nrf_wifi: Update RF parameters strings with CSP values
    [SHEL-2438] : TX power ceiling values and Back off values are updated
    according to the values given.
    
    Signed-off-by: Praveen Kankipati <Praveen.Kankipati@nordicsemi.no>

--- nrf_wifi/hw_if/hal/inc/fw/phy_rf_params.h
+++ nrf_wifi/hw_if/hal/inc/fw/phy_rf_params.h
@@ -153,23 +153,23 @@
 #define CSP_XO_VAL 0x2A
 
 /** Max TX power allowed for DSSS and OFDM in 2.4GHz band */
-#define CSP_MAX_TX_PWR_DSSS 0x58
-#define CSP_MAX_TX_PWR_LB_MCS7 0x48
-#define CSP_MAX_TX_PWR_LB_MCS0 0x50
+#define CSP_MAX_TX_PWR_DSSS 0x48
+#define CSP_MAX_TX_PWR_LB_MCS7 0x44
+#define CSP_MAX_TX_PWR_LB_MCS0 0x44
 
 /** Max TX power allowed for MCS7 for channels in the range,
  * 36 to 64, 96 to 132 and 136 to 177
  */
-#define CSP_MAX_TX_PWR_HB_LOW_CHAN_MCS7 0x40
-#define CSP_MAX_TX_PWR_HB_MID_CHAN_MCS7 0x40
-#define CSP_MAX_TX_PWR_HB_HIGH_CHAN_MCS7 0x40
+#define CSP_MAX_TX_PWR_HB_LOW_CHAN_MCS7 0x3C
+#define CSP_MAX_TX_PWR_HB_MID_CHAN_MCS7 0x3C
+#define CSP_MAX_TX_PWR_HB_HIGH_CHAN_MCS7 0x3C
 
 /** Max TX power allowed for MCS0 for channels in the range,
  * 36 to 64, 96 to 132 and 136 to 177
  */
-#define CSP_MAX_TX_PWR_HB_LOW_CHAN_MCS0 0x50
-#define CSP_MAX_TX_PWR_HB_MID_CHAN_MCS0 0x50
-#define CSP_MAX_TX_PWR_HB_HIGH_CHAN_MCS0 0x50
+#define CSP_MAX_TX_PWR_HB_LOW_CHAN_MCS0 0x3C
+#define CSP_MAX_TX_PWR_HB_MID_CHAN_MCS0 0x3C
+#define CSP_MAX_TX_PWR_HB_HIGH_CHAN_MCS0 0x3C
 
 /** Max chip temperature at which the TX power backoff to be applied. */
 #define CSP_MAX_CHIP_TEMP 0x43
@@ -189,14 +189,14 @@
 /** TX power backoff values to be applied in 2.4GHz and 5GHz band when
  * the voltage is less than NRF_WIFI_VBAT_VERYLOW
  */
-#define CSP_LB_VBT_LT_VLOW 0xF8
-#define CSP_HB_VBT_LT_VLOW 0xE8
+#define CSP_LB_VBT_LT_VLOW 0xFC
+#define CSP_HB_VBT_LT_VLOW 0xEC
 
 /** TX power backoff values to be applied in 2.4GHz and 5GHz band when
  * the voltage is less than NRF_WIFI_VBAT_LOW
  */
-#define CSP_LB_VBT_LT_LOW 0xFC
-#define CSP_HB_VBT_LT_LOW 0xF4
+#define CSP_LB_VBT_LT_LOW 0x00
+#define CSP_HB_VBT_LT_LOW 0xF8
 
 
 /** XO adjustment value */
