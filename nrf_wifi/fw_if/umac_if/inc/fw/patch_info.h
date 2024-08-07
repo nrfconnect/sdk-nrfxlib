5dfb4df080aef713b3f2ac21100301bf5eea15ed
Prasun Sinha <Prasun.Sinha@nordicsemi.no>
2024-08-07 14:44:55 +0530

    nrf_wifi: Update RPU patch to rev#b2838b3bec5
    PHY:
        [SHEL-2811] : Add support for capture timeout radio test command.
    
    Signed-off-by: Prasun Sinha <Prasun.Sinha@nordicsemi.no>

--- nrf_wifi/fw_if/umac_if/inc/fw/patch_info.h
+++ nrf_wifi/fw_if/umac_if/inc/fw/patch_info.h
@@ -61,7 +61,7 @@ struct nrf70_fw_image_info {
 #define RPU_FAMILY         (1)
 #define RPU_MAJOR_VERSION   (2)
 #define RPU_MINOR_VERSION   (11)
-#define RPU_PATCH_VERSION   (2)
+#define RPU_PATCH_VERSION   (3)
 
 /**
  * @}
