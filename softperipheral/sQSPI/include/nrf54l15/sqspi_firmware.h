70cb0b061260428810ce3ef56802179497bcc85b
Luis David Lopez <luis.david.lopez@nordicsemi.no>
2025-04-01 11:17:52 +0200

    softperipheral: rev cf28ddf86b89fb647ee876546770c2676b3bc63c
    softperipheral/CHANGELOG.rst contains the list of changes.
    
    Signed-off-by: Luis David Lopez <luis.david.lopez@nordicsemi.no>

---
+++ softperipheral/sQSPI/include/nrf54l15/sqspi_firmware.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef SQSPI_FIRMWARE_H__
+#define SQSPI_FIRMWARE_H__
+#include "sqspi_firmware_v0.1.0.h"
+#endif
