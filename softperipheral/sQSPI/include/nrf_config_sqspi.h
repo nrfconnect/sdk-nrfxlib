c8b1801fddde79521b0f034daa9b06e53971d636
Luis David Lopez <luis.david.lopez@nordicsemi.no>
2025-05-07 14:40:38 +0200

    softperipheral: rev c6e66aca635e9dece5992e1f9464da81e33ed6fa
    Using new naming convention: nrfx_qspi2 -> nrf_sqspi
    Using sQSPI 1.0.0 firmware (which now uses PIC)
    Deleting old driver files
    See CHANGELOG for more details
    
    Signed-off-by: Luis David Lopez <luis.david.lopez@nordicsemi.no>

---
+++ softperipheral/sQSPI/include/nrf_config_sqspi.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef NRF_CONFIG_SQSPI_H__
+#define NRF_CONFIG_SQSPI_H__
+
+/**
+ * @brief NRF_SQSPI_ENABLED
+ *
+ * Boolean. Accepted values: 0 and 1.
+ */
+#ifndef NRF_SQSPI_ENABLED
+#define NRF_SQSPI_ENABLED 0
+#endif
+
+/**
+ * @brief NRF_SQSPI_SP_INSTANCE
+ *
+ * Address value. 32 bits
+ */
+#ifndef NRF_SQSPI_SP_FIRMWARE_ADDR
+#define NRF_SQSPI_SP_FIRMWARE_ADDR 0xDEADBEEF
+#endif
+
+/**
+ * @brief NRF_SQSPI_MAX_NUM_DATA_LINES - Maximal number of data lines used by the QSPI peripheral.
+ *
+ * Integer value. Minimum: 1. Maximum: 8.
+ */
+#ifndef NRF_SQSPI_MAX_NUM_DATA_LINES
+#define NRF_SQSPI_MAX_NUM_DATA_LINES 2
+#endif
+
+/**
+ * @brief NRF_SQSPI_TRANSFERS_PER_REQUEST - Maximal number of transfers that can be passed in an array for a single request. It is the maximal value in the xfer_count parameter.
+ *
+ * Integer value.
+ */
+#ifndef NRF_SQSPI_TRANSFERS_PER_REQUEST
+#define NRF_SQSPI_TRANSFERS_PER_REQUEST 1
+#endif
+
+/**
+ * @brief NRF_SQSPI_DEFAULT_CONFIG_IRQ_PRIORITY
+ *
+ * Integer value. Minimum: 0. Maximum: 7.
+ */
+#ifndef NRF_SQSPI_DEFAULT_CONFIG_IRQ_PRIORITY
+#define NRF_SQSPI_DEFAULT_CONFIG_IRQ_PRIORITY NRFX_DEFAULT_IRQ_PRIORITY
+#endif
+
+/**
+ * @brief NRF_SQSPI_CONFIG_LOG_ENABLED
+ *
+ * Boolean. Accepted values: 0 and 1.
+ */
+#ifndef NRF_SQSPI_CONFIG_LOG_ENABLED
+#define NRF_SQSPI_CONFIG_LOG_ENABLED 0
+#endif
+
+/**
+ * @brief NRF_SQSPI_CONFIG_LOG_LEVEL
+ *
+ * Integer value.
+ * Supported values:
+ * - Off     = 0
+ * - Error   = 1
+ * - Warning = 2
+ * - Info    = 3
+ * - Debug   = 4
+ */
+#ifndef NRF_SQSPI_CONFIG_LOG_LEVEL
+#define NRF_SQSPI_CONFIG_LOG_LEVEL 3
+#endif
+
+#endif // NRF_CONFIG_SQSPI_H__
