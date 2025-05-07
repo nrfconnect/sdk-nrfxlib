238e8f66ad4d89ae45b7aff4f862e7340e414c62
Martin Tverdal <martin.tverdal@nordicsemi.no>
2025-05-07 11:42:33 +0300

    softdevice_controller: rev 99d902eabf10352b08d0d11ac97b3bf6bf49bf17
    CHANGELOG.rst contains the list of changes.
    
    Signed-off-by: Martin Tverdal <martin.tverdal@nordicsemi.no>

--- softdevice_controller/include/sdc_soc.h
+++ softdevice_controller/include/sdc_soc.h
@@ -35,7 +35,7 @@ extern "C" {
 #elif defined(NRF53_SERIES)
   /* PPI channels 3 - 12, for the nRF53 Series */
   #define SDC_DPPI_CHANNELS_USED_MASK     (0x00001ff8)
-#elif defined(NRF54L_SERIES)
+#elif defined(NRF54L_SERIES) || defined(NRF71_SERIES)
   #define SDC_DPPIC10_CHANNELS_USED_MASK  (0x00000ffe)
   #define SDC_DPPIC00_CHANNELS_USED_MASK  (0x0000000a)
   #define SDC_PPIB00_CHANNELS_USED_MASK   (0x0000000f)
