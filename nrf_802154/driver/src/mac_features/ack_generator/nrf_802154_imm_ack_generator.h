216d652b5e75bd79b9dbef4f64703285cb502bbf
Dawid Przybylo <dawid.przybylo@nordicsemi.no>
2026-04-03 11:01:49 +0200

    nrf_802154: rev 3c1dbd7510dd87450aa8fbb2746388fa06e3e5f3
    This commit updates revision of the nrf_802154 component.
    
    Signed-off-by: Dawid Przybylo <dawid.przybylo@nordicsemi.no>

--- nrf_802154/driver/src/mac_features/ack_generator/nrf_802154_imm_ack_generator.h
+++ nrf_802154/driver/src/mac_features/ack_generator/nrf_802154_imm_ack_generator.h
@@ -46,17 +46,21 @@
 
 #include "mac_features/nrf_802154_frame.h"
 
-/** Initializes the Immediate ACK generator module. */
+/**
+ * @brief Initialize the Immediate ACK generator module.
+ */
 void nrf_802154_imm_ack_generator_init(void);
 
-/** @brief Resets the Immediate ACK generator module.
+/**
+ * @brief Reset the Immediate ACK generator module.
  *
  * @note This function should be called for every received frame to be acknowledged before
  *       @ref nrf_802154_imm_ack_generator_create is called for that frame.
  */
 void nrf_802154_imm_ack_generator_reset(void);
 
-/** @brief Creates an Immediate ACK in response to the provided frame.
+/**
+ * @brief Create an Immediate ACK in response to the provided frame.
  *
  * @note Only those contents of the frame being acknowledged marked by @p p_frame_data as valid
  * are used for ACK generation. If any data necessary to generate an ACK is missing or marked as
@@ -71,7 +75,6 @@ void nrf_802154_imm_ack_generator_reset(void);
  *           of the created Immediate ACK frame, or NULL when the response cannot be
  *           created.
  */
-uint8_t * nrf_802154_imm_ack_generator_create(
-    const nrf_802154_frame_t * p_frame_data);
+uint8_t * nrf_802154_imm_ack_generator_create(const nrf_802154_frame_t * p_frame_data);
 
-#endif // NRF_802154_IMM_ACK_GENERATOR_H
+#endif /* NRF_802154_IMM_ACK_GENERATOR_H */
