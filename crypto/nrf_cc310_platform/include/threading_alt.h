75e1e56d90f21123b3450eafa8884e273788cb0a
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2026-05-13 13:51:24 +0300

    crypto: nrf_cc31[02]_platform: adjust mbedtls_threading_* mutexes
    Adjust the mutex typedef to the new TF-PSA-Crypto version.
    
    The header file changes are enough to get the builds to succeed
    but the global variables will need to be adjusted to match the
    type that TF-PSA-Crypto declares them with.
    This needs more changes in CryptoCell to actually work at runtime.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- crypto/nrf_cc310_platform/include/threading_alt.h
+++ crypto/nrf_cc310_platform/include/threading_alt.h
@@ -11,11 +11,14 @@
 #include "nrf_cc3xx_platform_mutex.h"
 
 /** @brief Alternate declaration of mbedtls mutex type
- * 
+ *
  * The RTOS may require allocation and freeing of resources
- * as the inner type of the mutex is represented by an 
+ * as the inner type of the mutex is represented by an
  * RTOS-friendly void pointer.
  */
-typedef nrf_cc3xx_platform_mutex_t mbedtls_threading_mutex_t;
+typedef nrf_cc3xx_platform_mutex_t mbedtls_platform_mutex_t;
+
+/** Unused, but needs to be defined. */
+typedef int mbedtls_platform_condition_variable_t;
 
 #endif /* MBEDTLS_THREADING_ALT_H */
