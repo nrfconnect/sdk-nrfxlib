98b212b4846a269ad32bb4053f878c8626d6739a
Nicolae Dicu <nicolae.dicu@nordicsemi.no>
2026-05-15 18:49:27 +0300

    treewide: Update license information
    Include fem libs under Nordic-5-Clause and fix license text.
    
    Signed-off-by: Nicolae Dicu <nicolae.dicu@nordicsemi.no>

--- nrf_modem/include/nrf_modem_dect.h
+++ nrf_modem/include/nrf_modem_dect.h
@@ -4,6 +4,8 @@
  * The information contained herein is confidential property of Nordic Semiconductor ASA.
  * The use, copying, transfer or disclosure of such information is prohibited except by
  * express written agreement with Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 /**
