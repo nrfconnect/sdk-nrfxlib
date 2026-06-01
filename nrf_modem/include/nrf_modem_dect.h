8c3eb1bb226274c4a0fdfe2ff8cccfea8c9347aa
Markus Rekdal <markus.rekdal@nordicsemi.no>
2026-06-01 08:48:37 +0200

    nrf_modem: Fix copyright notice in DECT header
    This file is not confidental, replace the text about confidentiality
    with a tag to the Nordic license.
    
    Signed-off-by: Markus Rekdal <markus.rekdal@nordicsemi.no>

--- nrf_modem/include/nrf_modem_dect.h
+++ nrf_modem/include/nrf_modem_dect.h
@@ -1,9 +1,7 @@
 /*
- * Copyright (c) 2026 Nordic Semiconductor ASA. All Rights Reserved.
+ * Copyright (c) 2026 Nordic Semiconductor ASA
  *
- * The information contained herein is confidential property of Nordic Semiconductor ASA.
- * The use, copying, transfer or disclosure of such information is prohibited except by
- * express written agreement with Nordic Semiconductor ASA.
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 /**
