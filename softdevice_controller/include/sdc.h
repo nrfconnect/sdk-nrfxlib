0b16863707c29d6417b47d06b771664418ef4425
Martin Tverdal <martin.tverdal@nordicsemi.no>
2026-08-19 08:52:52 +0300

    softdevice_controller: rev e08f6aa64876f1651756e92facbe2e707f9655a8
    CHANGELOG.rst contains the list of changes.
    
    Signed-off-by: Martin Tverdal <martin.tverdal@nordicsemi.no>

--- softdevice_controller/include/sdc.h
+++ softdevice_controller/include/sdc.h
@@ -160,7 +160,7 @@ extern "C" {
 
 /** @brief Auxiliary defines, not to be used outside of this file. */
 #define __MEM_MINIMAL_CENTRAL_LINK_SIZE    755
-#define __MEM_MINIMAL_PERIPHERAL_LINK_SIZE 871
+#define __MEM_MINIMAL_PERIPHERAL_LINK_SIZE 875
 #define __MEM_TX_BUFFER_OVERHEAD_SIZE 15
 #define __MEM_RX_BUFFER_OVERHEAD_SIZE 15
 
